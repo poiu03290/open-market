4c4b37e69c65ec6b85eb20f02897c7cccd16d74a
Hyuk Kwon <poiu03290@naver.com>
2023-02-06 23:53:02 +0900

    error handling

--- src/View/ProductDetail.js
+++ src/View/ProductDetail.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams, Link } from "react-router-dom";
 
 import { getProductDetail } from '../api/api';
